76669ee3e29814e1b22c31cd1deaaddbb42b0a49
Maximiliano Benítez <maximiliano.benitez@correo.ucu.edu.uy>
2021-10-26 23:01:36 -0300

    Implementación patrón  Visitor

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,24 +7,34 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Node n1 = new Node(1);
-            Node n2 = new Node(2);
-            Node n3 = new Node(3);
-            Node n4 = new Node(4);
-            Node n5 = new Node(5);
-            Node n6 = new Node(6);
-            Node n7 = new Node(7);
+            Node n1 = new Node(new Person("Raúl Benítez", 96));
+
+            Node n2 = new Node(new Person("Jorge Benítez", 78));
+            Node n3 = new Node(new Person("Roberto Benítez", 74));
+            Node n4 = new Node(new Person("Carlos Benítez", 69));
+
+            Node n5 = new Node(new Person("Luis Benítez", 59));
+            Node n6 = new Node(new Person("Francisco Benítez", 55));
+            Node n7 = new Node(new Person("Hugo Benítez", 52));
+
+            Node n8 = new Node(new Person("Maximiliano Benítez", 30));
 
             n1.AddChildren(n2);
             n1.AddChildren(n3);
-
             n2.AddChildren(n4);
-            n2.AddChildren(n5);
 
+            n2.AddChildren(n5);
             n3.AddChildren(n6);
             n3.AddChildren(n7);
 
-            // visitar el árbol aquí
+            n3.AddChildren(n8);
+
+            AddAge age = new AddAge();
+            n1.Accept(age);
+            int addAges = age.Ages;
+
+            Console.WriteLine(addAges);
+
         }
     }
 }
